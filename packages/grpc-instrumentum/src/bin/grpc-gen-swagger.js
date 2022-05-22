730143b46c73dad6a1699c541cb14e029035162e
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2022-05-22 06:06:39 +0300

    MacOS was supported in swagger generation

--- packages/grpc-instrumentum/src/bin/grpc-gen-swagger.js
+++ packages/grpc-instrumentum/src/bin/grpc-gen-swagger.js
@@ -9,9 +9,13 @@ const { execFileSync } = require("child_process");
 const env = { ...process.env };
 const pathKeyName = pathKey({ env });
 env[pathKeyName] = process.mainModule.paths
-  .map(x => path.join(x, "grpc-tools", "bin"))
+  .map((x) => path.join(x, "grpc-tools", "bin"))
   .concat(__dirname, env[pathKeyName])
   .join(path.delimiter);
 
-const args = ["-I", path.join(__dirname, "..", "include")].concat(process.argv.slice(2));
-execFileSync(`protoc${process.platform === "win32" ? ".exe" : ""}`, args, { env });
+const exeExt = process.platform === "win32" ? ".exe" : "";
+const pluginPath = path.join(__dirname, `protoc-gen-swagger-${process.platform}${exeExt}`);
+const includePath = path.join(__dirname, "..", "include");
+
+const args = ["--plugin=protoc-gen-swagger=" + pluginPath, "-I", includePath].concat(process.argv.slice(2));
+execFileSync(`protoc${exeExt}`, args, { env });
